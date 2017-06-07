36ac943c5c6972c6038e0e9cfb570668194588a4
Ben Fu <ben_fu1@yahoo.com>
2017-06-07 17:05:32 -0400

    Fix schemas

--- db/schemas.js
+++ db/schemas.js
@@ -1,22 +1,19 @@
 // Schemas
 
-class Customer {
-    donations = [];             // string of donation id's
-}
+// class Customer {
+//     donations = [];             // string of donation id's
+// }
 
-class Donation {
-    id = "";                     // unique id of donation - choose string or number
-    charities = ["ch1", "ch2"];  // list of charity id's
-    type = "fixed";              // fixed; percent
-    frequency = "once";          // once; monthly
-    dateStart = new Date();      // make sure to use Javascript date object here
-    dateEnd = new Date();
-}
+// class Donation {
+//     id = "";                     // unique id of donation - choose string or number
+//     charities = ["ch1", "ch2"];  // list of charity id's
+//     type = "fixed";              // fixed; percent
+//     frequency = "once";          // once; monthly
+//     dateStart = new Date();      // make sure to use Javascript date object here
+//     dateEnd = new Date();
+// }
 
-class Charity {
-    id = "";
-}
+// class Charity {
+//     id = "";
+// }
 
-module.exports.Customer = Customer;
-module.exports.Donation = Donation;
-module.exports.Charity = Charity;
