7c1d9295b815290c2ca545fe38e4f81f2f88af4e
Ben Fu <ben_fu1@yahoo.com>
2017-06-07 19:49:31 -0400

    Add customer routes

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 
 let Koa = require("koa");
+let BodyParser = require("koa-bodyparser");
+
 let Router = require("./router/router");
 
 const Database = require("./db/database");
@@ -9,24 +11,28 @@ let DBTest = require("./db/db-test");
 async function main() {
     var app = new Koa();
 
-    var router = new Router();
-    router.addRoutes();
+    
 
     // Configure middleware.
 
-    // First add routes.
-    app
-        .use(router.getKoaRouter().routes())
-        .use(router.getKoaRouter().allowedMethods());
+    // Body parser for requests.
+    app.use(BodyParser());
 
     // Other middleware
     app.use(async (ctx, next) => {
         const start = new Date;
-        return next().then(() => {
-            const ms = new Date - start;
-            console.log(`${ctx.method} ${ctx.url} - ${ms}`);
-        });
+        await next();
+        const ms = new Date - start;
+        console.log(`${ctx.method} ${ctx.url} - ${ms}`);
     });
+
+    // Routes.
+    var router = new Router();
+    router.addRoutes();
+    app
+        .use(router.getKoaRouter().routes())
+        .use(router.getKoaRouter().allowedMethods());
+
 
     await Database.connect();
 
--- router/routes/customer-routes.js
+++ router/routes/customer-routes.js
@@ -9,16 +9,35 @@ class CustomerRoutes {
 
     addRoutes(router) {
         router
-            .get("/customers", (ctx, next) => {
+            .get("/customers", async (ctx, next) => {
+                // If query exists, then use it
+                if (ctx.query) {
+                    console.log(ctx.query);
+                    ctx.body = await DBCustomer.find(ctx.query);
+                }
+                else {
+                    ctx.body = await DBCustomer.find();
+                }
                 
             })
 
-            .post("/customers", (ctx, next) => {
+            .post("/customers", async (ctx, next) => {
+                let reqBody = ctx.request.body;
+                if (reqBody instanceof Array) {
+                    console.log("creating multiple customers");
+                    ctx.body = await DBCustomer.insertMany(reqBody);
+                }
+                else {
+                    console.log("creating single customer");
+                    ctx.body = await DBCustomer.insertOne(reqBody);
+                }
 
             })
 
-            .get("/customers/:id", (ctx, next) => {
-
+            .get("/customers/:id", async (ctx, next) => {
+                ctx.body = await DBCustomer.find({
+                    id: ctx.params.id
+                });
             })
 
     }
