f034fe794cb5542fda7f3ab0fe17bd46876931b0
Ben Fu <ben_fu1@yahoo.com>
2017-06-08 00:41:36 -0400

    Add mock data scripts

--- db/db-test.js
+++ db/db-test.js
@@ -6,7 +6,11 @@ class DBTest {
 
     async test() {
         await DBMethods.find("customers");
-        Fetch.fetchConsumer();
+        // Fetch.fetchConsumer();
+
+        // await insertCustomersCharities();
+
+        
         // await DBCustomer.insertOne({ donations: ["d1", "d2"] });
         // await DBDonation.insertMany([{donations: ["d1", "d2"]}, {donations: ["d3", "d4"]}]);
         // await DBCharity.insertOne({ donations: ["d1", "d2"] });
@@ -24,6 +28,29 @@ class DBTest {
         // await DBDonation.updateMany({donations: ["d1", "d2"]}, {$set: {donations: ["d1"]}});
         // await DBCharity.deleteOne({donations: ["d1", "d2"]}, {$set: {donations: ["d1"]}});
         // await DBCustomer.deleteMany({donations: ["d1", "d2"]}, {$set: {donations: ["d1"]}});
+    }
+
+    async insertCustomersCharities() {
+        let json = require("../mock/customers.json");
+        let customers = json.results;
+        for (let customer of customers) {
+            customer = delete customer.address;
+        }
+        customers.splice(99);
+        await DBMethods.insertMany("customers", customers);
+
+        json = require("../mock/mockorgs.json");
+        let charities = json.orgs;
+        await DBMethods.insertMany("charities", charities);
+
+        console.log("done inserting");
+    }
+
+    async insertDonations() {
+        let customers = await DBMethods.find("customers");
+        for (let customer of customers) {
+            
+        }
     }
 
 }
